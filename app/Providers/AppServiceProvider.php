9f1ad37682cdb8853164ea375feb9f9bf73a9127
sihabuddinrifqi <syihabuddinrifqi@gmail.com>
2024-12-09 15:40:49 +0700

    first commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Vite::prefetch(concurrency: 3);
+    }
+    protected $listen = [
+        \App\Events\ChirpCreated::class => [
+            \App\Listeners\SendChirpNotification::class,
+        ],
+    ];
+}
